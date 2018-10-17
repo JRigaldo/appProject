da6f53411a48140cbcd5ad51a25958534545314a
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2018-10-17 18:17:50 +0200

    delete webpack project put cli + carousel tutorial grapfikart

---
+++ aurelia-cli/src/shell/shell.js
@@ -0,0 +1,27 @@
+
+export class Shell {
+
+  items = [
+	  {
+	  	"img" : "http://lorempicsum.com/futurama/350/200/1",
+	  	"text": "This is the first item"
+	  },
+	  {
+	  	"img" : "http://lorempicsum.com/futurama/350/200/2",
+	  	"text": "This is the second item"
+	  },
+	  {
+	  	"img" : "http://lorempicsum.com/futurama/350/200/3",
+	  	"text": "This is the third item"
+	  },
+	  {
+	  	"img" : "http://lorempicsum.com/futurama/350/200/4",
+	  	"text": "This is the third item"
+	  },
+	  {
+	  	"img" : "http://lorempicsum.com/futurama/350/200/5",
+	  	"text": "This is the third item"
+	  }
+	]
+
+}
