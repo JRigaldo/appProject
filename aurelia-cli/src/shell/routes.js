346f036bb417636491871e7ad02ac76e3d531cfc
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2018-10-18 16:08:41 +0200

    add bulma

---
+++ aurelia-cli/src/shell/routes.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    name: 'shell',
+    route: ['', 'shell'],
+    moduleId: 'shell/shell',
+    nav: true,
+    title: 'Shell',
+    settings: { iconClass: 'fa-shell' }
+  },
+  {
+    route: 'signin',
+    moduleId: 'signin/signin',
+    nav: true,
+    settings: { iconClass: 'fa-signin' }
+  }
+];
--- aurelia-cli/src/shell/shell.js
+++ aurelia-cli/src/shell/shell.js
@@ -1,27 +1,19 @@
+import routes from './routes';
 
 export class Shell {
 
-  items = [
-	  {
-	  	"img" : "http://lorempicsum.com/futurama/350/200/1",
-	  	"text": "This is the first item"
-	  },
-	  {
-	  	"img" : "http://lorempicsum.com/futurama/350/200/2",
-	  	"text": "This is the second item"
-	  },
-	  {
-	  	"img" : "http://lorempicsum.com/futurama/350/200/3",
-	  	"text": "This is the third item"
-	  },
-	  {
-	  	"img" : "http://lorempicsum.com/futurama/350/200/4",
-	  	"text": "This is the third item"
-	  },
-	  {
-	  	"img" : "http://lorempicsum.com/futurama/350/200/5",
-	  	"text": "This is the third item"
-	  }
-	]
+  constructor(){
+    this.isSelected = false;
+  }
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.map(routes);
+  }
+
+  toggleMenu() {
+		this.isSelected = !this.isSelected;
+    console.log(this.isSelected);
+	}
 
 }
