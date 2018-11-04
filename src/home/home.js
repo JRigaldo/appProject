983adfe14106ca173956c3ece6264b37e8f0bca4
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2018-11-04 17:59:04 +0100

    desktop css layout

--- src/home/home.js
+++ src/home/home.js
@@ -9,34 +9,34 @@ export class Home{
 
 
   attached(){
-    this.slider();
-    this.masonry();
+    // this.slider();
+    // this.masonry();
   }
 
-  slider(){
-    setTimeout(() => {
-      $('.slider').slick()
-    }, 1000);
-  }
-
-  masonry() {
-    let grid = document.querySelector('.grid')
-    let msnry = new Masonry(grid, {
-      // itemSelector: '.grid-item',
-      // percentPosition: true,
-      // columnWidth: 280,
-      // gutter: 50,
-      // transitionDuration: 0,
-      // initLayout: false
-      itemSelector: '.grid-item',
-      columnWidth: 160
-    })
-    msnry.once('layoutComplete', () => {
-      grid.classList.add('load')
-    })
-
-    msnry.layout()
-  }
+  // slider(){
+  //   setTimeout(() => {
+  //     $('.slider').slick()
+  //   }, 1000);
+  // }
+  //
+  // masonry() {
+  //   let grid = document.querySelector('.grid')
+  //   let msnry = new Masonry(grid, {
+  //     // itemSelector: '.grid-item',
+  //     // percentPosition: true,
+  //     // columnWidth: 280,
+  //     // gutter: 50,
+  //     // transitionDuration: 0,
+  //     // initLayout: false
+  //     itemSelector: '.grid-item',
+  //     columnWidth: 160
+  //   })
+  //   msnry.once('layoutComplete', () => {
+  //     grid.classList.add('load')
+  //   })
+  //
+  //   msnry.layout()
+  // }
 
 
 }
---
+++ src/resources/elements/tags-list-header.js
@@ -0,0 +1,30 @@
+export class tagsListHeader{
+
+  constructor(options = {}){
+    this.options = Object.assign({}, {
+      slidesVisibles: 4
+    }, options);
+  }
+
+  attached(){
+    let children = [].slice.call(this.tagList.children);
+    let items = children.map(child => {
+      let childClass = document.createElement('button');
+      childClass.setAttribute('class', 'tag-class');
+      childClass.appendChild(child);
+      this.tagList.appendChild(childClass);
+
+      return childClass;
+    });
+
+
+    let ratio = this.tagList.children.length / this.options.slidesVisibles;
+    console.log(ratio);
+    this.tagList.style.width = (ratio * 100) + '%';
+
+    items.forEach((item) => {
+      item.style.width = ((100 / this.options.slidesVisibles) / ratio ) + '%';
+    });
+  }
+
+}
