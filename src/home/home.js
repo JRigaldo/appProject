67854797d9289ef440998a62f3a3777443d18b64
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2018-11-07 13:27:25 +0100

    add masonry grid

--- src/home/home.js
+++ src/home/home.js
@@ -1,40 +1,43 @@
-import $ from 'jquery';
-import Masonry from 'masonry-layout';
-
 export class Home{
-
-  constructor(){
-  }
-
-
-  attached(){
-    this.masonry();
-  }
-
-  masonry() {
-    let grid = document.querySelector('.grid')
-    let msnry = new Masonry(grid, {
-      itemSelector: '.grid-item',
-      columnWidth: '.grid-sizer',
-      gutter: '.gutter-sizer',
-      percentPosition: true
-    })
-    // msnry.once('layoutComplete', () => {
-    //   grid.classList.add('load')
-    // })
-
-    msnry.layout()
-  }
-
-
-
+  
 }
 
-// // itemSelector: '.grid-item',
-// // percentPosition: true,
-// // columnWidth: 280,
-// // gutter: 50,
-// // transitionDuration: 0,
-// // initLayout: false
-// itemSelector: '.grid-item',
-// columnWidth: 160
+// import $ from 'jquery';
+// import Masonry from 'masonry-layout';
+//
+// export class Home{
+//
+//   constructor(){
+//   }
+//
+//
+//   attached(){
+//     this.masonry();
+//   }
+//
+//   masonry() {
+//     let grid = document.querySelector('.grid')
+//     let msnry = new Masonry(grid, {
+//       // itemSelector: '.grid-item',
+//       // columnWidth: '.grid-sizer',
+//       // gutter: '.gutter-sizer',
+//       // percentPosition: true,
+//       // // itemSelector: '.grid-item',
+//       // // percentPosition: true,
+//       // // columnWidth: 280,
+//       // // gutter: 50,
+//       // // transitionDuration: 0,
+//       // // initLayout: false
+//       // itemSelector: '.grid-item',
+//       // columnWidth: 160
+//     })
+//     msnry.once('layoutComplete', () => {
+//       grid.classList.add('load')
+//     })
+//
+//     msnry.layout()
+//   }
+//
+//
+//
+// }
