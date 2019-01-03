5ee5e687cbd309a6e1ca3c0a266f744ab9d74124
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2019-01-03 10:36:19 +0100

    forgot to save the merge conflict change

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ export function configure(aurelia) {
     .standardConfiguration()
     .feature('resources')
     .plugin('aurelia-validation')
-<<<<<<< HEAD
-    .plugin('aurelia-dialog');
-=======
     .plugin('aurelia-i18n', instance => {
       instance.i18next.use(Backend.with(aurelia.loader));
         return instance.setup({
@@ -21,7 +18,6 @@ export function configure(aurelia) {
           debug: false
         });
     });
->>>>>>> i18n
 
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
